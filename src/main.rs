d7b8e39e7dbc5bc31445bfd452663d51182e36ab
NotLe0n <notle0n75@gmail.com>
2022-06-15 17:53:54 +0200

    added min window size
    and removed a wasm dependency

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,28 @@ fn main() {
 	// Log to stdout (if you run with `RUST_LOG=debug`).
 	tracing_subscriber::fmt::init();
 
-	let native_options = eframe::NativeOptions::default();
+	use egui::Vec2;
+	let options = eframe::NativeOptions {
+		always_on_top: false,
+		maximized: false,
+		decorated: true,
+		drag_and_drop_support: true,
+		icon_data: None,
+		initial_window_pos: None,
+		initial_window_size: Option::from(Vec2::new(500.0, 200.0)),
+		min_window_size: Option::from(Vec2::new(500.0, 150.0)),
+		max_window_size: None,
+		resizable: true,
+		transparent: false,
+		vsync: true,
+		multisampling: 0,
+		depth_buffer: 0,
+		stencil_buffer: 0,
+	};
+	
 	eframe::run_native(
-		"YouTube Downloader GUI",
-		native_options,
+		"YouTube Downloader",
+		options,
 		Box::new(|cc| Box::new(youtube_downloader::GUI::new(cc))),
 	);
 }
